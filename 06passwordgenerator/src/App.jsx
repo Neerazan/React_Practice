2fcef6d9f504c48c3ee67a7baa0362869dcd144d
Neerajan Dhakal <86935895+Neerazan@users.noreply.github.com>
2024-03-09 19:25:28 +0545

    password generator UI completed

---
+++ 06passwordgenerator/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react"
+import "./App.css"
+
+function App() {
+    const [length, setLength] = useState(8)
+
+    return (
+        <>
+            <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+                <h1 className="text-white text-center my-3">
+                    Password Generator
+                </h1>
+                <div className="flex shadow rounded-lg overflow-hidden mb-4">
+                    <input
+                        type="text"
+                        value=""
+                        className="outline-none w-full py-1 px-3"
+                        placeholder="Password"
+                        readOnly
+                    />
+                    <button
+                        onClick=""
+                        className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+                    >
+                        copy
+                    </button>
+                </div>
+
+                <div className="flex text-sm gap-x-2">
+                  <div className="flex items-center gap-x-1">
+                    <input
+                        type="range"
+                        min={6}
+                        max={100}
+                        value={length}
+                        className="cursor-pointer"
+                        onChange={(e) => {
+                            setLength(e.target.value)
+                        }}
+                    />
+                    <label htmlFor="">Length: {length}</label>
+                  </div>
+                  <div className="flex items-center gap-x-1">
+                    <input
+                        type="checkbox"
+                        defaultChecked
+                        id="numberInput"
+                        onChange=""
+                    />
+                    <label htmlFor="">Numbers</label>
+                  </div>
+                  <div className="flex items-center gap-x-1">
+                    <input
+                        type="checkbox"
+                        defaultChecked
+                        id="charInput"
+                        onChange=""
+                    />
+                    <label htmlFor="">Special Chars</label>
+                  </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default App
