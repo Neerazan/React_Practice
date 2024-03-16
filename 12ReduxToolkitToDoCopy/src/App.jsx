5df307f82c9fc1b380f9bd6ddfc2fcf54269e366
Neerajan Dhakal <86935895+Neerazan@users.noreply.github.com>
2024-03-16 09:43:40 +0545

    12ReduxToolkitTodoCopay with update functionality store

---
+++ 12ReduxToolkitToDoCopy/src/App.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react"
+
+import "./App.css"
+import AddTodo from "./components/Todo"
+import TodoList from "./components/TodoList"
+import { useSelector } from "react-redux"
+
+function App() {
+    const allTasks = useSelector((state) => state.todos)
+    const [editValue, setEditValue] = useState()
+
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(allTasks))
+    }, [allTasks])
+
+    return (
+        <>
+            <h1>ReduxToolkit TODO App (🔥)</h1>
+            <AddTodo editValue={editValue}/>
+            <TodoList setEditValue = {setEditValue}/>
+        </>
+    )
+}
+
+export default App
---
+++ 12ReduxToolkitToDoCopy/src/components/Todo.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { addTodo, updateTodo } from "../app/features/todo/todoSlice"
+
+function AddTodo({ editValue }) {
+    const dispatch = useDispatch()
+    const [input, setInput] = useState("")
+    const [button, setButton] = useState("Add")
+
+    useEffect(() => {
+        if (editValue) {
+            setInput(editValue.text)
+            setButton("Update")
+        }
+    }, [editValue])
+
+    const addUpdateTodoHandler = (event) => {
+        event.preventDefault()
+        const value = input
+
+        if (value !== "") {
+            if (button === "Update") {
+                dispatch(
+                    updateTodo({
+                        id: editValue.id,
+                        text: input,
+                    })
+                )
+                setButton("Add")
+                setInput("")
+            } else {
+                dispatch(addTodo(value))
+                setInput("")
+            }
+        }
+    }
+
+    return (
+        <form onSubmit={addUpdateTodoHandler} className="space-x-3 mt-12">
+            <input
+                type="text"
+                className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                placeholder="Enter a Todo..."
+                value={input}
+                onChange={(event) => setInput(event.target.value)}
+            />
+
+            <button
+                type="submit"
+                className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+            >
+                {button}
+            </button>
+        </form>
+    )
+}
+
+export default AddTodo
