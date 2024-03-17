7bb2b886fea7626898acd7cf9e2fb75d65c5a62d
Neerajan Dhakal <86935895+Neerazan@users.noreply.github.com>
2024-03-17 20:27:35 +0545

    User guide added

--- 12ReduxToolkitToDoCopy/src/App.jsx
+++ 12ReduxToolkitToDoCopy/src/App.jsx
@@ -7,13 +7,36 @@ import { useSelector } from "react-redux"
 
 function App() {
     const allTasks = useSelector((state) => state.todos)
-    
+    let userVisited = localStorage.getItem("todos")
+
     useEffect(() => {
         localStorage.setItem("todos", JSON.stringify(allTasks))
     }, [allTasks])
 
+    const handleUserVisited = (target) => {
+        let parent = target.closest('#userGuide')
+        if(parent){
+            parent.remove()
+        }
+    }
+
     return (
         <>
+            {userVisited == null ? (
+                <div className="mb-3 animate-bounce" id="userGuide">
+                    <span className="bg-blue-600 ps-2 py-2 rounded-md hover:ring-2">
+                        Click todo to mark as completed
+                        <span 
+                            className="ms-1 p-2 cursor-pointer bg-blue-700 rounded-e-md hover:bg-blue-800"
+                            onClick={(e) => handleUserVisited(e.target)}
+                        >
+                            ok
+                        </span>
+                    </span>
+                </div>
+            ) : null}
+
+
             <h1>ReduxToolkit TODO App (🔥)</h1>
             <AddTodo />
             <TodoList />
