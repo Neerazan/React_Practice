847e5b993cd164eb550be2fdadb6c4bb70e53300
Neerajan Dhakal <86935895+Neerazan@users.noreply.github.com>
2024-04-13 23:29:00 +0545

    Getting data using axios

--- HandleAPIS/frontend/src/App.jsx
+++ HandleAPIS/frontend/src/App.jsx
@@ -1,10 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 import './App.css'
 
 function App() {
   const [products, setProducts] = useState([])
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    // ()() this is Immideatly invoked function
+    setError(false)
+    ;(async () => {
+      try {
+        setLoading(true)
+        const response = await axios.get('/api/products')
+        console.log(response.data);
+        setProducts(response.data)
+        setLoading(false)
+      } catch (error) {
+        setError(true)
+        setLoading(false)
+      }
+    })()
+  }, [])
+
+  if (error) {
+    return <h1>Something Went Wrong.</h1>
+  }
+
+  if(loading){
+    return <h1>Loading...</h1>
+  }
 
   return (
     <>
